f0f23d2b674ddc8ca228ea14e5001d2c7404f43a
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-10-25 16:21:46 +0530

    RMX1801: Switch to walleye oreo fp
    Signed-off-by: baibhab <fakebai34@gmail.com>

--- init/init_sdm660.cpp
+++ init/init_sdm660.cpp
@@ -113,6 +113,6 @@ void vendor_load_properties()
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 
     // fingerprint
-    property_override("ro.build.description", "sunfish-user 11 RQ3A.211001.001 7641976 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/sunfish/sunfish:11/RQ3A.211001.001/7641976:user/release-keys");
+    property_override("ro.build.description", "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
 }
