d5309757e1ccdbd95320debbb00360f69df57181
vijaymalav564 <jaymalav10@gmail.com>
2021-10-29 10:49:30 +0000

    Revert "RMX1801: Add missing 64-bit interface required for IMS"
    This reverts commit ec727f778476ecca268dada448acae9b97d1c4f6.

--- device.mk
+++ device.mk
@@ -176,15 +176,13 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
-    android.hardware.camera.device@3.4:64 \
-    android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4:64 \
+    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5:64 \
+    android.hardware.camera.provider@2.5 \
     libstdc++.vendor \
     Snap \
-    vendor.qti.hardware.camera.device@1.0:64
+    vendor.qti.hardware.camera.device@1.0
 
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
